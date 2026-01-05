aa312a04454cc9882cc5d443d1acc13ed7757920
Xaver Gruber <xaver.max.gruber@gmail.com>
2026-01-05 15:46:16 +0100

    add argparse, copy files to remote

--- shared/scripts/deploy_webservice.py
+++ shared/scripts/deploy_webservice.py
@@ -2,6 +2,8 @@ import paramiko
 from scp import SCPClient
 from dotenv import load_dotenv
 import os
+import argparse
+from urllib.parse import urlparse
 
 
 def main():
@@ -24,16 +26,61 @@ def main():
     ssh.exec_command(
         'powershell -Command "New-Item -ItemType Directory -Path C:\\production -Force"'
     )
-    
+   
+    remote_deploy_path = "C:/production/"
     with SCPClient(ssh.get_transport()) as scp:
-       scp.put("setupWindowsWebservice.ps1", "C:/production/")
-       scp.put("setupWindowsPorts.ps1", "C:/production/")
-    
-    
+       scp.put("setupWindowsWebservice.ps1", remote_deploy_path)
+       scp.put("setupWindowsPorts.ps1", remote_deploy_path)
+   
+    args = parse_arguments()
+
+    if args.path:
+        path = os.path.expanduser(args.path)
+        path = os.path.abspath(path)
+
+        if not os.path.exists(path):
+            parser.error(f"Path does not exist: {path}")
+
+        print(f"Path selected: {path}")
+
+    if args.url:
+        parsed = urlparse(args.url)
+        if not parsed.scheme:
+            parser.error(f"Invalid URL: {args.url}")
+        sys.exit("download url not yet implemented")
+        print(f"URL selected: {args.url}")
+
+    basename = os.path.basename(os.path.normpath(path))
+    with SCPClient(ssh.get_transport()) as scp:
+        scp.put(path, remote_deploy_path, recursive=True)
+    if basename != "Artifacts":
+        cmd = f'powershell -Command Remove-Item -Path "{remote_deploy_path}/Artifacts" -Recurse -Force'
+        ssh.exec_command(cmd)
+        cmd = f'powershell -Command Rename-Item -Path "{remote_deploy_path}/{basename}" -NewName "Artifacts"'
+        ssh.exec_command(cmd)
+
     #ssh.exec_command("cd C:/production && setupWindowsPorts ")
     
     ssh.close()
-    
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Use either --path or --url")
+
+    group = parser.add_mutually_exclusive_group(required=True)
+
+    group.add_argument(
+        "--path",
+        help="Local filesystem path"
+    )
+
+    group.add_argument(
+        "--url",
+        help="Remote URL (http, https, ftp, etc.)"
+    )
+
+    return parser.parse_args()
+
+
 
 def create_ssh_client(host, user, password=None, key_file=None, port=22):
     client = paramiko.SSHClient()
